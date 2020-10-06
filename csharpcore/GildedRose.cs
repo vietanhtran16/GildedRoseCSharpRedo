90fb8da35081ce7839a0e017fbdf13e0d73c3212
Viet Anh Tran <tranvietanh1996@gmail.com>
2020-10-07 06:43:56 +1300

    Make use of item name enum

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -14,11 +14,11 @@ namespace csharpcore
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Name != ItemName.AgedBried && Items[i].Name != ItemName.BackstagePass)
                 {
                     if (Items[i].Quality > 0)
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (Items[i].Name != ItemName.Sulfuras)
                         {
                             Items[i].Quality = Items[i].Quality - 1;
                         }
@@ -30,7 +30,7 @@ namespace csharpcore
                     {
                         Items[i].Quality = Items[i].Quality + 1;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Name == ItemName.BackstagePass)
                         {
                             if (Items[i].SellIn < 11)
                             {
@@ -51,20 +51,20 @@ namespace csharpcore
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (Items[i].Name != ItemName.Sulfuras)
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name != ItemName.AgedBried)
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Name != ItemName.BackstagePass)
                         {
                             if (Items[i].Quality > 0)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (Items[i].Name != ItemName.Sulfuras)
                                 {
                                     Items[i].Quality = Items[i].Quality - 1;
                                 }
--- csharpcore/Program.cs
+++ csharpcore/Program.cs
@@ -10,26 +10,26 @@ namespace csharpcore
             Console.WriteLine("OMGHAI!");
 
             IList<Item> Items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item {Name = ItemName.DexterityVest, SellIn = 10, Quality = 20},
+                new Item {Name = ItemName.AgedBried, SellIn = 2, Quality = 0},
+                new Item {Name = ItemName.Elixir, SellIn = 5, Quality = 7},
+                new Item {Name = ItemName.Sulfuras, SellIn = 0, Quality = 80},
+                new Item {Name = ItemName.Sulfuras, SellIn = -1, Quality = 80},
                 new Item
                 {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    Name = ItemName.BackstagePass,
                     SellIn = 15,
                     Quality = 20
                 },
                 new Item
                 {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    Name = ItemName.BackstagePass,
                     SellIn = 10,
                     Quality = 49
                 },
                 new Item
                 {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    Name = ItemName.BackstagePass,
                     SellIn = 5,
                     Quality = 49
                 },
